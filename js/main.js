85aee509d6fc6dc297c09b8100ce9c21bdb5fd56
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-04-05 15:55:36 +0300

    ModalJS/Wrote a basic show modal window

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 const modalWindow = document.querySelector('.modal');
-const modalBtn = document.querySelectorAll('')
+const modalBtn = document.querySelectorAll('.modal__btn');
+
+// Перебор всех кнопок с классом .modal__btn
+modalBtn.forEach((item)=> {
+	// На каждую кнопку по клику "вешается" обработчик
+	item.addEventListener('click', () => {
+		modalWindow.style.display = 'grid';
+	});
+	
+})
