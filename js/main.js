f9cc3edb5a80657c9e8b14f97bf0bc4c7493ab67
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-04-05 16:24:09 +0300

    ModalJS/Wrote a modal window close by click outside

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const modalWindow = document.querySelector('.modal');
 const modalBtn = document.querySelectorAll('.modal__btn');
 
+// -- Появление модального окна
 // Перебор всех кнопок с классом .modal__btn
 modalBtn.forEach((item)=> {
 	// На каждую кнопку по клику "вешается" обработчик
@@ -8,4 +9,14 @@ modalBtn.forEach((item)=> {
 		modalWindow.style.display = 'grid';
 	});
 	
-})
+});
+
+
+// --- Исчезновение (закрытие) модального окна
+modalWindow.addEventListener('click', (e) => {
+	// Запись в переменную isModal по клику на высплывающее окно
+	const isModal = e.target.closest('.modal-window__wrapper');
+	if(!isModal) {
+		modalWindow.style.display = 'none';
+	}
+});
