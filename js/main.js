7e57fa8a16a5c741846b29a9b6be85334fcdfb62
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-04-15 12:41:08 +0300

    MST/Set the scroll line delay if PopUp is close

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const body = document.querySelector('body');
 const modalWindow = document.querySelector('.modal');
 const modalClose = document.querySelector('.modal__close');
 const modalBtn = document.querySelectorAll('.modal__btn');
-const timeout = 800;
+const timeout = 500;
 const lockPadding = document.querySelectorAll('.lock-padding');
 
 // -- Появление модального окна
@@ -13,17 +13,14 @@ modalBtn.forEach(item=> {
 		body.classList.add('noscroll');
 	});
 });
-console.log(window.innerWidth);
-console.log(body.offsetWidth);
 
 // -- Исчезновение (закрытие) модального окна по клику вне модального окна
 modalWindow.addEventListener('click', (e) => {
 	// Получение по клику (e - событие) в переменную isModal внешнего поля (затем. фон)
 	const isModal = e.target.closest('.modal-window__wrapper');
 	if(!isModal) {
-		removeScrollIndent();
 		modalWindow.classList.remove('modal-open');
-		body.classList.remove('noscroll');
+		removeScrollIndent();
 	}
 });
 
@@ -31,23 +28,18 @@ modalWindow.addEventListener('click', (e) => {
 modalClose.addEventListener('click', () => {
 	modalWindow.classList.remove('modal-open');
 	removeScrollIndent();
-	body.classList.remove('noscroll');
 });
 
 // Компенсация сдвига по гор. вправо при появлении модального окна
 function addScrollIndent() {
 	const lockPaddingValue = window.innerWidth - body.offsetWidth + 'px';
-		
-	console.log(window.innerWidth);
-	console.log(body.offsetWidth);
-	console.log(lockPaddingValue);
-
 	body.style.paddingRight = lockPaddingValue;
 }
 
 // Компенсация сдвига по гор. влево при закрытии модального окна
 function removeScrollIndent() {  
-    body.style.paddingRight = '0px';
-	setTimeout(function () {
-    }, timeout);
+	setTimeout(function() {
+		body.style.paddingRight = '0px';
+		body.classList.remove('noscroll');
+	}, timeout);
 }
