d1e12097c598031a89739e99e92feb8e5ea88d2e
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-04-19 14:54:11 +0300

    FN/Setting the scroll btn script

--- js/main.js
+++ js/main.js
@@ -53,15 +53,15 @@ document.addEventListener ('keydown', function (e) {
 
 // -- Кнопка для прокрутки вверх
 // Получаю в константу кнопку (стрелку)
-const scrollBtn = document.querySelector('.show__btn');
+const scrollBtn = document.querySelector('.scroll_arrow');
 window.onscroll = () => {
 	// Появление кнопки при вертикальной прокрутке вниз (более чем на 400 px)
 	if(window.scrollY > 400) {
-		scrollBtn.classList.remove('show__btn-hide');
+		scrollBtn.classList.remove('scroll__btn-hide');
 	}
 	// Исчезновение кнопки при вертикальной прокрутке вверх (менее 400 px)
 	else if (window.scrollY < 400) {
-		scrollBtn.classList.add('show__btn-hide');
+		scrollBtn.classList.add('scroll__btn-hide');
 	}
 };
 
