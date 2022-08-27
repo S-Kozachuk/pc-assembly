594b7c0e2ba791afe51ed4f65cb30f0d5a2a2787
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-08-27 16:59:24 +0300

    Fix hendler code

--- php/mail.php
+++ php/mail.php
@@ -1,40 +1,25 @@
-<?php 
+<?php
 
-require_once('..\\libs/phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
+//Script Foreach
+$c = true;
 
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
+// For POST method only!
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+// Save Basic Form parametrs
+$project_name = trim($_POST["project_name"]);
+$admin_email  = trim($_POST["admin_email"]);
+$email_from  = trim($_POST["email_from"]);
+// $form_subject = trim($_POST["form_subject"]);
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.yandex.ru'; 																						// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'kozachuk-dev@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'goyqjmuxryikqwzm'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
-
-$mail->setFrom('kozachuk-dev@yandex.ru'); // от кого будет уходить письмо?
-$mail->addAddress('skozachuk91@yandex.ru'); // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
-$mail->AltBody = '';
-
-	success: function (html) {
-		$('#contacts-form').slideUp(800);
-		$('#answer').html(html);
+// Serialize form fields - that filled-in by User
+foreach ( $_POST as $key => $value ) {
+	if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" && $key != "email_from" ) {
+		$message .= "
+		" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+		<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+		<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+	</tr>
+	";
 	}
 }
 
@@ -75,11 +60,4 @@ $headers = "MIME-Version: 1.0" . PHP_EOL .
 					Наш специалист свяжется с Вами в ближайшее время.
 				</h2>
 		</div>";
-?>
-
-
-	echo '<script type="text/javascript">',
-	 'ajaxFormSubmit();'
-     '</script>'
-;
 ?>
