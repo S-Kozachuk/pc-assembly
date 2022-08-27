0e0588468191cfbf5be2af9a7682a81a46205ffa
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-08-27 09:02:47 +0300

    Changed the setting in PHPMailer

--- php/mail.php
+++ php/mail.php
@@ -17,18 +17,12 @@ $mail->Username = 'kozachuk-dev@yandex.ru'; // Ваш логин от почты
 $mail->Password = 'goyqjmuxryikqwzm'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
-
-$mail->setFrom('kozachuk-dev@yandex.ru'); // от кого будет уходить письмо?
+$mail->setFrom('kozachuk-dev@yandex.ru', 'PC-Assembly.ru'); // от кого будет уходить письмо?
 $mail->addAddress('skozachuk91@yandex.ru'); // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+$mail->isHTML(true); // Set email format to HTML
 
-$mail->Subject = 'Заявка с тестового сайта';
+$mail->Subject = 'Order from PC-Assembly.ru';
 $mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
 $mail->AltBody = '';
 
