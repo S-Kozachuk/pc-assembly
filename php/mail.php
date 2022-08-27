a7b00aac7849141bd749460629b19a6d05f7aa61
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-08-27 17:25:47 +0300

    Deleted the email password form PHP-handler

--- php/mail.php
+++ php/mail.php
@@ -14,7 +14,7 @@ $mail->isSMTP(); // Set mailer to use SMTP
 $mail->Host = 'smtp.yandex.ru'; 																						// Specify main and backup SMTP servers
 $mail->SMTPAuth = true; // Enable SMTP authentication
 $mail->Username = 'kozachuk-dev@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'goyqjmuxryikqwzm'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Password = 'enter email password'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 $mail->setFrom('kozachuk-dev@yandex.ru', 'PC-Assembly.ru'); // от кого будет уходить письмо?
